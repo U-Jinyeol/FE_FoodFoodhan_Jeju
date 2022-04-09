6fe9018679c69785b7abd5fb200122f3f6ccb97a
U-Jinyeol <100745412+U-Jinyeol@users.noreply.github.com>
2022-04-09 10:56:12 +0900

    메인 카드 추가

--- src/elements/Grid.js
+++ src/elements/Grid.js
@@ -2,15 +2,29 @@ import React from "react";
 import styled from "styled-components";
 
 const Grid = (props) => {
-  const { is_flex, width, margin, padding, bg, children, center } = props;
+  const {
+    height,
+    is_flex1,
+    is_flex2,
+    is_flex3,
+    width,
+    margin,
+    padding,
+    bg,
+    children,
+    center,
+  } = props;
 
   const styles = {
-    is_flex: is_flex,
+    is_flex1: is_flex1,
+    is_flex2: is_flex2,
+    is_flex3: is_flex3,
     width: width,
     margin: margin,
     padding: padding,
     bg: bg,
     center: center,
+    height: height,
   };
   return (
     <React.Fragment>
@@ -21,24 +35,39 @@ const Grid = (props) => {
 
 Grid.defaultProps = {
   chidren: null,
-  is_flex: false,
+  is_flex1: false,
+  is_flex2: false,
+  is_flex3: false,
   width: "100%",
   padding: false,
   margin: false,
   bg: false,
   center: false,
+  height: "100%",
 };
 
 const GridBox = styled.div`
+  height: ${(props) => props.width};
   width: ${(props) => props.width};
-  height: 100%;
   box-sizing: border-box;
   ${(props) => (props.padding ? `padding: ${props.padding};` : "")}
   ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
   ${(props) => (props.bg ? `background-color: ${props.bg};` : "")}
   ${(props) =>
-    props.is_flex
-      ? `display: flex; align-items: center; justify-content: space-between; `
+    props.is_flex1
+      ? `display: flex; align-items: center; justify-content: space-around `
+      : ""}
+      ${(props) => (props.center ? `text-align: center;` : "")}
+
+  ${(props) =>
+    props.is_flex2
+      ? `display: flex; align-items: center; justify-content: center `
+      : ""}
+      ${(props) => (props.center ? `text-align: center;` : "")}
+
+      ${(props) =>
+    props.is_flex3
+      ? `display: flex; align-items: center; justify-content: left `
       : ""}
       ${(props) => (props.center ? `text-align: center;` : "")}
 `;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,66 @@
 import React from "react";
+import Grid from "../elements/Grid";
+import Text from "../elements/Text";
+import styled from "styled-components";
+import { FaRegCommentDots, FaRegHeart } from "react-icons/fa";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <>
+      <Grid is_flex1 margin="40px 40px">
+        <CardBox>
+          <p>제주도 할망 라면</p>
+          <Img src="https://img.insight.co.kr/static/2020/09/26/700/img_20200926141402_cd1rk7ql.jpg" />
+
+          <Grid is_flex3>
+            <FaRegCommentDots size={20}></FaRegCommentDots>
+            <Text margin="5px">10</Text>
+            <FaRegHeart size={20}></FaRegHeart>
+            <Text margin="5px">20</Text>
+          </Grid>
+          <p>성산</p>
+          <p>
+            해녀분이 직접 잡아 당일 재료로 끓여주시는 자연 오션뷰 라면집
+            해녀분이 직접 잡아 당일 재료로 끓여주시는 자연 오션뷰 라면집
+            해녀분이 직접 잡아 당일 재료로 끓여주시는 자연 오션뷰 라면집
+          </p>
+          <p>자세한 정보는 카드 클릭!</p>
+        </CardBox>
+
+        <CardBox>
+          <p>제주도 흑돼지</p>
+          <Img src="https://static.hubzum.zumst.com/hubzum/2018/03/02/13/f0e963a82e974cc5a4fea79ea94928fd.jpg" />
+
+          <Grid is_flex3>
+            <FaRegCommentDots size={20}></FaRegCommentDots>
+            <Text margin="5px">10</Text>
+            <FaRegHeart size={20}></FaRegHeart>
+            <Text margin="5px">20</Text>
+          </Grid>
+          <p>애월</p>
+          <p>
+            제주도 하면 바로 흑돼지!! 식감과 맛이 일품! 제주도 하면 바로
+            흑돼지!! 식감과 맛이 일품! 제주도 하면 바로 흑돼지!! 식감과 맛이
+            일품!
+          </p>
+          <p>자세한 정보는 카드 클릭!</p>
+        </CardBox>
+      </Grid>
+    </>
+  );
 };
+
+const CardBox = styled.div`
+  border: 0.5px solid gray;
+  max-width: 400px;
+  width: 100%;
+`;
+
+const Img = styled.img`
+  background-size: cover;
+  width: 400px;
+  height: 300px;
+  object-fit: cover;
+`;
 
 export default Main;
